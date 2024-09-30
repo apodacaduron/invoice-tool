52a3ba4acdad0203262cf4be93efe28ed528670f
Daniel Apodaca <47479471+apodacaduron@users.noreply.github.com>
2024-09-30 13:41:51 -0600

    setup prime vue and tailwind css

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,21 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+import './index.css'
 
-createApp(App).mount("#app");
+const app = createApp(App);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            cssLayer: {
+                name: 'primevue',
+                order: 'tailwind-base, primevue, tailwind-utilities'
+            }
+        }
+    }
+});
+
+app.mount('#app');
+
