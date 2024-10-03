a5bafd6a3a21d90059048e8efcaf931b4fbb7f83
Daniel Apodaca <47479471+apodacaduron@users.noreply.github.com>
2024-10-02 19:33:03 -0600

    remove unused files

--- src/components/MobileInvoiceView.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import InvoiceForm from './InvoiceForm.vue';
-</script>
-
-<template>
-    <div class="flex">
-        <InvoiceForm class="mx-auto" />
-    </div>
-</template>
